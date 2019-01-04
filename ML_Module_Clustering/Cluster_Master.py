726dced6d4f805bfcd7905762bf3aa1231e59e44
drkkgy <drkkgy@gmail.com>
2019-01-04 10:31:45 +0530

    adding ML CLustering

---
+++ ML_Module_Clustering/Cluster_Master.py
@@ -0,0 +1,12 @@
+"""
+Master Clustering Module
+Clustering Technique under Implementation is DB -Scan
+Author:- Ankit Yadav [15SCSE101296]
+"""
+
+import sys
+sys.path.append('X-Shield-0.0.0.1\Driver Module\driver_scripts\cpu_usage.py')
+# importing the dependent files
+import cpu_usage as cu
+print(cu.cpu_usage(23))
+# Data buffer
