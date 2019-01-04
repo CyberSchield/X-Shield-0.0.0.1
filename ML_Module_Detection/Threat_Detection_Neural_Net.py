d1c8bb5fc2c3f661839999e3098df39333430b78
ankit <drkkgy@gmail.com>
2019-01-05 00:21:52 +0530

    NN Model Basic Architeture completed and validated

--- ML_Module_Detection/Threat_Detection_Neural_Net.py
+++ ML_Module_Detection/Threat_Detection_Neural_Net.py
@@ -11,13 +11,16 @@ Cyber-Shield
 
 import numpy as np
 import sklearn as skl
+import matplotlib.pyplot as plt
 
+Input_Data = [2.5,4.5,3.0,1.0,5.3,2.3,5.1,4.0,7.0,5.4,2.3]
+Labels_of_Input_Data = [1,2,2,3,3,2,5,5,3,5,4]
 # Neural Network Model ---------------------------------------
 # ---------------Neural Network Architecture---------------------
 # Configuration of neural Network Architecture
-X = Input_Data # Source Data
-Y = Labels_of_Input_Data # Source known result
-Layers_Dimensions = [X.shape[1],3,4,Y.shape[1]]  #First value is input and last is output and the middle values are hidden layers
+X = np.asarray(Input_Data) # Source Data
+Y = np.asarray(Labels_of_Input_Data) # Source known result
+Layers_Dimensions = np.asarray([X.shape[1],3,4,Y.shape[1]])  #First value is input and last is output and the middle values are hidden layers
 Layer_Order = ['S','R','R','R','L']
 # -------------------------------------------------
 
@@ -30,12 +33,12 @@ def Initilize_params(Layers_Dimensions):
     parameters_Hidden_Layer = {}
     L = len(Layers_Dimensions)
     for i in range(1, L):
-        parameters_Hidden_Layer['Wx' + str(i)] = np.random.randn(Layers_Dimensions[i], Layers_Dimensions[i-1])*0.01
-        parameters_Hidden_Layer['Bx' + str(i)] = np.zeros((Layers_Dimensions[i], 1))
+        parameters_Hidden_Layer['W' + str(i)] = np.random.randn(Layers_Dimensions[i], Layers_Dimensions[i-1])*0.01
+        parameters_Hidden_Layer['b' + str(i)] = np.zeros((Layers_Dimensions[i], 1))
 
         # analysing the dimensions and giving error of wrong dimension matrix
-        assert (parameters_Hidden_Layer['Wx' + str(i)].shape == (parameters_Hidden_Layer[i]),parameters_Hidden_Layer[i-1])
-        assert (parameters_Hidden_Layer['Bx' + str(i)].shape == (parameters_Hidden_Layer[i]),1)
+        assert (parameters_Hidden_Layer['W' + str(i)].shape == (parameters_Hidden_Layer[i]),parameters_Hidden_Layer[i-1])
+        assert (parameters_Hidden_Layer['b' + str(i)].shape == (parameters_Hidden_Layer[i]),1)
 
     ## Checking the dimension of the matrix raises error for some anamalyous behaviour
     parameters = parameters_Hidden_Layer
@@ -84,10 +87,10 @@ def Linear_Activation_forward(A_prev, W , b , activation): ## activation = 1[Sig
         A, activation_cache = max_func(Z)
 
     # Checking the matrix dimensions
-        assert (A.shape == (W.shape[0], A_prev.shape[1]))
-        cache = (linear_cache, activation_cache)
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
 
-        return  A, cache
+    return  A, cache
 # Main consolidated forward propagation function
 def Main_Forward_propagation_Function(X,parameters):
     activation = 0
@@ -105,7 +108,7 @@ def Main_Forward_propagation_Function(X,parameters):
 
         # implementing the final result layer
 
-    AL, cache = Linear_Activation_forward(A, parameters['W'+str(L)], parameters['b'+str(L)],0)
+    AL, cache = Linear_Activation_forward(A, parameters['W'+str(L)], parameters['b'+str(L)],2)
     caches.append(cache)
 
     # Checking the dimension of result matrix
@@ -145,22 +148,112 @@ def linear_backward(dZ, cache):
 
     return dA_prev, dW, db
 
+# Linear Activation backward
 
 def linear_activation_backward(dA, cache, activation):
 
     linear_cache, activation_cache = cache
 
-    if activation == 1:
+    if activation == 2:
 
         dZ = dRelu(dA, activation_cache)
         dA_prev, dW, db = linear_backward(dZ, linear_cache)
 
-    elif activation == 2:
+    elif activation == 1:
 
         dZ = dsigmoid(dA, activation_cache)
         dA_prev, dW, db = linear_backward(dZ, linear_cache)
 
     return dA_prev, dW, db
+
+# Main Linear Backward Model
+
+def L_model_backward(AL, Y, caches):
+
+    grads = {}
+    L = len(caches)  # the number of layers
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape)  # shaping Y as the  same shape as AL
+
+    # Initializing the backpropagation
+
+
+    dAL = -(np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+
+
+
+    current_cache = caches[L - 1]
+    grads["dA" + str(L - 1)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL,current_cache,2)
+
+    # Loop from l=L-2 to l=0
+    for l in reversed(range(L - 1)):
+        if Layer_Order[l] == 'S':
+            activation = 1
+        elif Layer_Order[l] == 'R':
+            activation = 2
+        current_cache = caches[l]
+        dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA" + str(L - 1)], current_cache, activation)
+        grads["dA" + str(l)] = dA_prev_temp
+        grads["dW" + str(l + 1)] = dW_temp
+        grads["db" + str(l + 1)] = db_temp
+
+
+    return grads
+
+# Updating Parameters Helper Function
+
+def update_parameters(parameters, grads, learning_rate):
+
+    L = len(parameters) // 2  # number of layers in the neural network
+
+
+    for l in range(1, L):
+        parameters["W" + str(l + 1)] = parameters['W' + str(l)] - learning_rate * (grads['dW' + str(l)])
+        parameters["b" + str(l + 1)] = parameters['b' + str(l)] - learning_rate * (grads['db' + str(l)])
+
+    return parameters
+
+
+## Training the NN Model Function
+
+def Train_NN_Model(X, Y, Layers_Dimensions, learning_rate = 0.0075, num_iterations = 3000,):
+    np.random.seed(1)
+    costs = []  # keep track of cost
+
+    parameters = Initilize_params(Layers_Dimensions)
+
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+
+
+        AL, caches = Main_Forward_propagation_Function(X, parameters)
+
+        # Compute cost.
+
+        cost = compute_cost(AL, Y)
+
+        # Backward propagation.
+
+        grads = L_model_backward(AL, Y, caches)
+
+
+        # Update parameters.
+
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+      ##------------Debuger Code-------------------------------------------
+        # Print the cost every 100 training example
+        print(cost)
+
+    # plot the cost
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per tens)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+
+    return parameters
+
 
 
 
