59e79b352000ad8294576100d815ce7817b0c6a4
drkkgy <drkkgy@gmail.com>
2019-01-04 12:21:11 +0530

    NN Model Architecture function ready

--- ML_Module_Detection/Threat_Detection_Neural_Net.py
+++ ML_Module_Detection/Threat_Detection_Neural_Net.py
@@ -7,4 +7,19 @@ Author :- Ankit yadav
 Cyber-Shield
 '''
 
-# Importing
+# Importing Packages
+
+import numpy as np
+import sklearn as skl
+
+## Neural Network Model ---------------------------------------
+
+# Configuration of neural Network Architecture
+
+def Neural_network_Architecture(input_size_x,Hidden_Layer_size,output_layer_size_y):
+    x_size = input_size_x.shape[0]                                                    ## Pulling in the input data size values
+    y_size = output_layer_size_y.shape[1]                                             ## pulling the values for the label dimension
+    return (x_size,Hidden_Layer_size,y_size)
+
+
+
