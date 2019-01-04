5784371cb6351b91586ab09ab2f336d92da67ed7
drkkgy <drkkgy@gmail.com>
2019-01-04 13:02:04 +0530

    updated the architecture configuration of the NN

--- ML_Module_Detection/Threat_Detection_Neural_Net.py
+++ ML_Module_Detection/Threat_Detection_Neural_Net.py
@@ -16,10 +16,30 @@ import sklearn as skl
 
 # Configuration of neural Network Architecture
 
-def Neural_network_Architecture(input_size_x,Hidden_Layer_size,output_layer_size_y):
+def Neural_network_Architecture(input_size_x,Hidden_Layer_size,Hidden_Layer_No,output_layer_size_y):
     x_size = input_size_x.shape[0]                                                    ## Pulling in the input data size values
     y_size = output_layer_size_y.shape[1]                                             ## pulling the values for the label dimension
-    return (x_size,Hidden_Layer_size,y_size)
+    return (x_size,Hidden_Layer_size,Hidden_Layer_No,y_size)
 
 
+# Initialising the input parameters
 
+def Initilize_params(x_size,Hidden_Layer_size,Hidden_layer_No,y_size):
+    Wx = np.random.randn(Hidden_Layer_size,x_size,Hidden_layer_No)*0.01 # Initialising the weight matrix for the data
+    Bx = np.zeros((Hidden_Layer_size,1,Hidden_layer_No)) # initialising the bias matrix for the data
+
+    Wy = np.random.randn(y_size,Hidden_Layer_size)*0.01 # initialising the weight matrix of label portion
+    By = np.zeros((y_size,1))
+
+    ## Checking the dimension of the matrix raises error for some anamalyous behaviour
+
+    assert (Wx.shape == (Hidden_Layer_size, x_size,Hidden_layer_No))
+    assert (Bx.shape == (Hidden_Layer_size, 1,Hidden_layer_No))
+    assert (Wy.shape == (y_size, Hidden_Layer_size))
+    assert (By.shape == (y_size, 1))
+
+    parameters = {"Wx": Wx, "bx": Bx, "Wy": Wy, "by": By}
+    return parameters
+
+c = Initilize_params(2,4,3,3)
+print(c)
