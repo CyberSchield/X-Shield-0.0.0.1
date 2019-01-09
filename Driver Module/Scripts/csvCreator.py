3863b30fe27665718f4ac81342daa33f796b6c43
Yash-Jn <me.yashjn@gmail.com>
2019-01-09 12:27:42 +0530

    Add files via upload

---
+++ Driver Module/Scripts/csvCreator.py
@@ -0,0 +1,11 @@
+import csv
+
+def csvCreator(process_list):
+	row=["cpu_usage","ram_usage","user_type","process_priority","process_state"]
+	with open('Data.csv','a') as csvFile:
+		writer=csv.writer(csvFile)
+		writer.writerow(row)
+		for p in process_list:
+			row=[p.cpu_usage,p.ram_usage,p.user_type,p.process_priority,p.process_state]
+			writer.writerow(row)
+	csvFile.close()
