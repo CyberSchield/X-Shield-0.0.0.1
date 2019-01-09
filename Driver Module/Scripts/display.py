e7dcb8c3c64e617d0c60ce2fac17bf283c3039df
Yash-Jn <me.yashjn@gmail.com>
2019-01-09 12:19:35 +0530

    Delete display.py

--- Driver Module/Scripts/display.py
+++
@@ -1,3 +0,0 @@
-def display(process_list):
-	for p in process_list:
-		print(str(p.process_id)+"    "+str(p.ram_usage)+"   "+str(p.cpu_usage)+"     "+str(p.process_priority)+"     "+str(p.user_type)+"      "+str(p.process_state))
