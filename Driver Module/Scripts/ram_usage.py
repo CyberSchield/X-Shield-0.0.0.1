2e887fb02f0d77dcbcfe09ab27d2fe393e798d35
Yash-Jn <me.yashjn@gmail.com>
2019-03-18 15:14:11 +0530

    Delete ram_usage.py

--- Driver Module/Scripts/ram_usage.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-def ram_usage(pid):
-	temp=subprocess.Popen(['ps','-o','%mem','-p',str(pid)],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-	ram_usage=str(temp.stdout.read())[8:-3].strip()
-	if ram_usage=="":
-		ram_usage=0.0
-	else:
-		ram_usage=float(ram_usage)
-	return ram_usage
