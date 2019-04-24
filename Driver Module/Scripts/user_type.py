75153c14684b57b2c655db45bb24b047a5c6ae67
Yash-Jn <me.yashjn@gmail.com>
2019-04-24 20:14:19 +0530

    Delete user_type.py

--- Driver Module/Scripts/user_type.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-def user_type(pid):
-	temp=subprocess.Popen(['ps','-o','user','-p',str(pid)],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-	user_type=str(temp.stdout.read())[8:-3].strip()
-	if user_type=="" or user_type=="root":
-		user_type=1
-	else:
-		user_type=0
-	return user_type
